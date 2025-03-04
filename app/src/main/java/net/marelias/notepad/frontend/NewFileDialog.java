0a29de9c6aa74f59238d2d61d0b87673404bfac0
elyahw <136118735+elyahw@users.noreply.github.com>
2025-03-04 19:34:30 +0000

    Delete unused form xml; comment out old new file form

--- app/src/main/java/net/marelias/notepad/frontend/NewFileDialog.java
+++ app/src/main/java/net/marelias/notepad/frontend/NewFileDialog.java
@@ -78,201 +78,201 @@ public class NewFileDialog extends DialogFragment {
         return dialog;
     }
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final File file = (File) getArguments().getSerializable(EXTRA_DIR);
-        final boolean allowCreateDir = getArguments().getBoolean(EXTRA_ALLOW_CREATE_DIR);
-        final LayoutInflater inflater = LayoutInflater.from(getActivity());
-        return makeDialog(file, allowCreateDir, inflater);
-    }
+//    @NonNull
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        final File file = (File) getArguments().getSerializable(EXTRA_DIR);
+//        final boolean allowCreateDir = getArguments().getBoolean(EXTRA_ALLOW_CREATE_DIR);
+//        final LayoutInflater inflater = LayoutInflater.from(getActivity());
+//        return makeDialog(file, allowCreateDir, inflater);
+//    }
 
-    @SuppressLint("SetTextI18n")
-    private AlertDialog makeDialog(final File basedir, final boolean allowCreateDir, LayoutInflater inflater) {
-        final Activity activity = getActivity();
-        final AppSettings appSettings = ApplicationObject.settings();
-        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(inflater.getContext(), R.style.Theme_AppCompat_DayNight_Dialog_Rounded);
-        final View root = inflater.inflate(R.layout.new_file_dialog, null);
-
-        final EditText titleEdit = root.findViewById(R.id.new_file_dialog__name);
-        final EditText formatEdit = root.findViewById(R.id.new_file_dialog__name_format);
-        final TextView formatSpinner = root.findViewById(R.id.new_file_dialog__name_format_spinner);
-
-        titleEdit.requestFocus();
-        new Handler().postDelayed(new GsContextUtils.DoTouchView(titleEdit), 200);
-
-        titleEdit.setFilters(new InputFilter[]{GsContextUtils.instance.makeFilenameInputFilter()});
-
-        // Build a list of available formats
-        // -----------------------------------------------------------------------------------------
-        final List<FormatRegistry.Format> formats = GsCollectionUtils.map(
-                NEW_FILE_FORMATS, t -> GsCollectionUtils.selectFirst(FormatRegistry.FORMATS, f -> f.format == t));
-
-        // Setup title format spinner and actions
-        // -----------------------------------------------------------------------------------------
-        final ArrayAdapter<String> formatAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_spinner_dropdown_item) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-
-                TextView textView = (TextView) view.findViewById(android.R.id.text1);
-
-                String item = getItem(position);
-                if (item != null && item.equals(formatEdit.getText().toString())) {
-                    textView.setTypeface(null, Typeface.BOLD);
-                } else {
-                    textView.setTypeface(null, Typeface.NORMAL);
-                }
-
-                return view;
-            }
-        };
-
-        formatAdapter.addAll(appSettings.getTitleFormats());
-
-        final ListPopupWindow formatPopup = new ListPopupWindow(activity);
-        formatPopup.setAdapter(formatAdapter);
-        formatPopup.setAnchorView(formatEdit);
-
-        formatEdit.setText(formatAdapter.getItem(0));
-        formatPopup.setOnItemClickListener((parent, view, position, id) -> {
-            String selectedItem = formatAdapter.getItem(position);
-            formatEdit.setText(selectedItem);
-            formatPopup.dismiss();
-            formatAdapter.notifyDataSetChanged();
-        });
-
-        formatSpinner.setOnClickListener(v -> formatPopup.show());
-        formatEdit.setOnClickListener(v -> formatPopup.show());
-
-        // Setup template spinner and action
-        // -----------------------------------------------------------------------------------------
-        final List<Pair<String, String>> templates = appSettings.getBuiltinTemplates();
-
-        dialogBuilder.setView(root);
-
-        // Setup button click actions
-        // -----------------------------------------------------------------------------------------
-
-        final GsCallback.s0 getTitle = () -> {
-            final String title = titleEdit.getText().toString().trim();
-
-            // Elyahw: This is what is done on the press of the okay button in new file dialog. For options see [AppSettings.java]
-            String format = formatEdit.getText().toString().trim();
-
-            if (title.isEmpty())//format.isEmpty() &&
-            {
-                format = "`yyyyMMdd'_'hhMMss`"; // the rest of options are in AppSettings.java
-            }
-            else if (!title.isEmpty() && !format.contains("{{title}}"))
-            {
-                format += "_{{title}}";
-            }
-
-            return TextViewUtils.interpolateSnippet(format, title, "").trim();
-        };
-
-        final GsCallback.s0 getTitle_folder = () -> {
-            final String title = titleEdit.getText().toString().trim();
-            String format = formatEdit.getText().toString().trim();
-            if (title.isEmpty())//format.isEmpty() &&
-            {
-                format = "`yyyyMMdd'_'hhMMss`"; // the rest of options are in AppSettings.java
-            }
-            else
-            {
-                format = "{{title}}";
-            }
-            return TextViewUtils.interpolateSnippet(format, title, "").trim();
-        };
-
-        final MarkorContextUtils cu = new MarkorContextUtils(getContext());
-        dialogBuilder.setNegativeButton(R.string.cancel, (dialogInterface, i) -> dialogInterface.dismiss());
-        dialogBuilder.setPositiveButton(getString(android.R.string.ok), (dialogInterface, i) -> {
-            // final FormatRegistry.Format fmt = formats.get(typeSpinner.getSelectedItemPosition());
-
-            final String title = getTitle.callback();
-            String fileName = GsFileUtils.getFilteredFilenameWithoutDisallowedChars(title + ".txt");
-
-            // Get template string
-            // -------------------------------------------------------------------------------------
-            final String template;
-            //template = "";
-            template = "-----\nNote Title\n-----\n"; // possible to enter a template here..
-
-            final Pair<String, Integer> content = getTemplateContent(template, title);
-            // -------------------------------------------------------------------------------------
-
-            final File file = new File(basedir, fileName);
-
-            // Most of the logic we want is in the document class so we just reuse it
-            final Document document = new Document(file);
-
-            // These are done even if the file isn't created
-            final String titleFormat = formatEdit.getText().toString().trim();
-            final FormatRegistry.Format fmt = formats.get(0);
-            appSettings.setNewFileDialogLastUsedType(fmt.format);
-
-            if (!file.exists() || file.length() <= GsContextUtils.TEXTFILE_OVERWRITE_MIN_TEXT_LENGTH) {
-                document.saveContent(activity, content.first, cu, true);
-
-                // We only make these changes if the file did not already exist
-                appSettings.setDocumentFormat(document.path, fmt.format);
-                appSettings.setLastEditPosition(document.path, content.second);
-                appSettings.setNewFileDialogLastUsedExtension(".txt");
-
-                callback(file);
-
-            } else if (file.canWrite()) {
-                callback(file);
-            } else {
-                Toast.makeText(activity, R.string.failed_to_create_backup, Toast.LENGTH_LONG).show();
-            }
-
-            dialogInterface.dismiss();
-        });
-
-        // Folder creation button:
-        dialogBuilder.setNeutralButton(R.string.folder, (dialogInterface, i) -> {
-            final String title = getTitle_folder.callback();
-            final String dirName = GsFileUtils.getFilteredFilenameWithoutDisallowedChars(title);
-
-            final File f = new File(basedir, dirName);
-
-            final String titleFormat = formatEdit.getText().toString().trim();
-
-            if (cu.isUnderStorageAccessFolder(getContext(), f, true)) {
-                DocumentFile dof = cu.getDocumentFile(getContext(), f, true);
-                if (dof != null && dof.exists()) {
-                    callback(f);
-                }
-            } else if (f.isDirectory() || f.mkdirs()) {
-                callback(f);
-            }
-            dialogInterface.dismiss();
-        });
-
-        if (!allowCreateDir) {
-            dialogBuilder.setNeutralButton("", null);
-        }
-
-        // elyahw (makes no difference)
-        // titleEdit.requestFocus();
-        /////////////////
-        // Initial creation - loop through and set type
-        final int lastUsedType = appSettings.getNewFileDialogLastUsedType();
-        final List<Integer> indices = GsCollectionUtils.indices(formats, f -> f.format == lastUsedType);
-
-        final AlertDialog dialog = dialogBuilder.show();
-        final Window win = dialog.getWindow();
-        if (win != null) {
-            win.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
-            win.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN | WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-        }
-        titleEdit.post(titleEdit::requestFocus);
-        ///////////////// elyahw
-        return dialog;
-    }
+//    @SuppressLint("SetTextI18n")
+//    private AlertDialog makeDialog(final File basedir, final boolean allowCreateDir, LayoutInflater inflater) {
+//        final Activity activity = getActivity();
+//        final AppSettings appSettings = ApplicationObject.settings();
+//        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(inflater.getContext(), R.style.Theme_AppCompat_DayNight_Dialog_Rounded);
+//        final View root = inflater.inflate(R.layout.new_file_dialog, null);
+//
+//        final EditText titleEdit = root.findViewById(R.id.new_file_dialog__name);
+//        final EditText formatEdit = root.findViewById(R.id.new_file_dialog__name_format);
+//        final TextView formatSpinner = root.findViewById(R.id.new_file_dialog__name_format_spinner);
+//
+//        titleEdit.requestFocus();
+//        new Handler().postDelayed(new GsContextUtils.DoTouchView(titleEdit), 200);
+//
+//        titleEdit.setFilters(new InputFilter[]{GsContextUtils.instance.makeFilenameInputFilter()});
+//
+//        // Build a list of available formats
+//        // -----------------------------------------------------------------------------------------
+//        final List<FormatRegistry.Format> formats = GsCollectionUtils.map(
+//                NEW_FILE_FORMATS, t -> GsCollectionUtils.selectFirst(FormatRegistry.FORMATS, f -> f.format == t));
+//
+//        // Setup title format spinner and actions
+//        // -----------------------------------------------------------------------------------------
+//        final ArrayAdapter<String> formatAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_spinner_dropdown_item) {
+//            @Override
+//            public View getView(int position, View convertView, ViewGroup parent) {
+//                View view = super.getView(position, convertView, parent);
+//
+//                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+//
+//                String item = getItem(position);
+//                if (item != null && item.equals(formatEdit.getText().toString())) {
+//                    textView.setTypeface(null, Typeface.BOLD);
+//                } else {
+//                    textView.setTypeface(null, Typeface.NORMAL);
+//                }
+//
+//                return view;
+//            }
+//        };
+//
+//        formatAdapter.addAll(appSettings.getTitleFormats());
+//
+//        final ListPopupWindow formatPopup = new ListPopupWindow(activity);
+//        formatPopup.setAdapter(formatAdapter);
+//        formatPopup.setAnchorView(formatEdit);
+//
+//        formatEdit.setText(formatAdapter.getItem(0));
+//        formatPopup.setOnItemClickListener((parent, view, position, id) -> {
+//            String selectedItem = formatAdapter.getItem(position);
+//            formatEdit.setText(selectedItem);
+//            formatPopup.dismiss();
+//            formatAdapter.notifyDataSetChanged();
+//        });
+//
+//        formatSpinner.setOnClickListener(v -> formatPopup.show());
+//        formatEdit.setOnClickListener(v -> formatPopup.show());
+//
+//        // Setup template spinner and action
+//        // -----------------------------------------------------------------------------------------
+//        final List<Pair<String, String>> templates = appSettings.getBuiltinTemplates();
+//
+//        dialogBuilder.setView(root);
+//
+//        // Setup button click actions
+//        // -----------------------------------------------------------------------------------------
+//
+//        final GsCallback.s0 getTitle = () -> {
+//            final String title = titleEdit.getText().toString().trim();
+//
+//            // Elyahw: This is what is done on the press of the okay button in new file dialog. For options see [AppSettings.java]
+//            String format = formatEdit.getText().toString().trim();
+//
+//            if (title.isEmpty())//format.isEmpty() &&
+//            {
+//                format = "`yyyyMMdd'_'hhMMss`"; // the rest of options are in AppSettings.java
+//            }
+//            else if (!title.isEmpty() && !format.contains("{{title}}"))
+//            {
+//                format += "_{{title}}";
+//            }
+//
+//            return TextViewUtils.interpolateSnippet(format, title, "").trim();
+//        };
+//
+//        final GsCallback.s0 getTitle_folder = () -> {
+//            final String title = titleEdit.getText().toString().trim();
+//            String format = formatEdit.getText().toString().trim();
+//            if (title.isEmpty())//format.isEmpty() &&
+//            {
+//                format = "`yyyyMMdd'_'hhMMss`"; // the rest of options are in AppSettings.java
+//            }
+//            else
+//            {
+//                format = "{{title}}";
+//            }
+//            return TextViewUtils.interpolateSnippet(format, title, "").trim();
+//        };
+//
+//        final MarkorContextUtils cu = new MarkorContextUtils(getContext());
+//        dialogBuilder.setNegativeButton(R.string.cancel, (dialogInterface, i) -> dialogInterface.dismiss());
+//        dialogBuilder.setPositiveButton(getString(android.R.string.ok), (dialogInterface, i) -> {
+//            // final FormatRegistry.Format fmt = formats.get(typeSpinner.getSelectedItemPosition());
+//
+//            final String title = getTitle.callback();
+//            String fileName = GsFileUtils.getFilteredFilenameWithoutDisallowedChars(title + ".txt");
+//
+//            // Get template string
+//            // -------------------------------------------------------------------------------------
+//            final String template;
+//            //template = "";
+//            template = "-----\nNote Title\n-----\n"; // possible to enter a template here..
+//
+//            final Pair<String, Integer> content = getTemplateContent(template, title);
+//            // -------------------------------------------------------------------------------------
+//
+//            final File file = new File(basedir, fileName);
+//
+//            // Most of the logic we want is in the document class so we just reuse it
+//            final Document document = new Document(file);
+//
+//            // These are done even if the file isn't created
+//            final String titleFormat = formatEdit.getText().toString().trim();
+//            final FormatRegistry.Format fmt = formats.get(0);
+//            appSettings.setNewFileDialogLastUsedType(fmt.format);
+//
+//            if (!file.exists() || file.length() <= GsContextUtils.TEXTFILE_OVERWRITE_MIN_TEXT_LENGTH) {
+//                document.saveContent(activity, content.first, cu, true);
+//
+//                // We only make these changes if the file did not already exist
+//                appSettings.setDocumentFormat(document.path, fmt.format);
+//                appSettings.setLastEditPosition(document.path, content.second);
+//                appSettings.setNewFileDialogLastUsedExtension(".txt");
+//
+//                callback(file);
+//
+//            } else if (file.canWrite()) {
+//                callback(file);
+//            } else {
+//                Toast.makeText(activity, R.string.failed_to_create_backup, Toast.LENGTH_LONG).show();
+//            }
+//
+//            dialogInterface.dismiss();
+//        });
+//
+//        // Folder creation button:
+//        dialogBuilder.setNeutralButton(R.string.folder, (dialogInterface, i) -> {
+//            final String title = getTitle_folder.callback();
+//            final String dirName = GsFileUtils.getFilteredFilenameWithoutDisallowedChars(title);
+//
+//            final File f = new File(basedir, dirName);
+//
+//            final String titleFormat = formatEdit.getText().toString().trim();
+//
+//            if (cu.isUnderStorageAccessFolder(getContext(), f, true)) {
+//                DocumentFile dof = cu.getDocumentFile(getContext(), f, true);
+//                if (dof != null && dof.exists()) {
+//                    callback(f);
+//                }
+//            } else if (f.isDirectory() || f.mkdirs()) {
+//                callback(f);
+//            }
+//            dialogInterface.dismiss();
+//        });
+//
+//        if (!allowCreateDir) {
+//            dialogBuilder.setNeutralButton("", null);
+//        }
+//
+//        // elyahw (makes no difference)
+//        // titleEdit.requestFocus();
+//        /////////////////
+//        // Initial creation - loop through and set type
+//        final int lastUsedType = appSettings.getNewFileDialogLastUsedType();
+//        final List<Integer> indices = GsCollectionUtils.indices(formats, f -> f.format == lastUsedType);
+//
+//        final AlertDialog dialog = dialogBuilder.show();
+//        final Window win = dialog.getWindow();
+//        if (win != null) {
+//            win.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+//            win.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN | WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+//        }
+//        titleEdit.post(titleEdit::requestFocus);
+//        ///////////////// elyahw
+//        return dialog;
+//    }
 
     private void callback(final File file) {
         try {
