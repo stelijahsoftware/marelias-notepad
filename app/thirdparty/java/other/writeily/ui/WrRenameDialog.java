c4818d5f783c142f726346a3f36796e9965b1493
Elyahw <136118735+elyahw@users.noreply.github.com>
2025-03-07 23:51:15 +0000

    Minor correction to renaming

--- app/thirdparty/java/other/writeily/ui/WrRenameDialog.java
+++ app/thirdparty/java/other/writeily/ui/WrRenameDialog.java
@@ -131,7 +131,7 @@ public class WrRenameDialog extends DialogFragment {
         } else if (file_name.contains("-")) {
             name_from = file_name.indexOf('-');
         }
-        editText.setSelection(name_from,name_to);
+        editText.setSelection(name_from+1,name_to);
 
         dialogBuilder.setPositiveButton(getString(android.R.string.ok), null);
         dialogBuilder.setNegativeButton(getString(R.string.cancel), null);
