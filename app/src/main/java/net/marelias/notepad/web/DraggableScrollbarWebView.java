dae254685d8673c711e37c84fade22c91cbfcfca
Elyahw <136118735+elyahw@users.noreply.github.com>
2025-03-29 09:11:48 +0000

    Remove possibly unneeded scrollbar code

--- app/src/main/java/net/marelias/notepad/web/DraggableScrollbarWebView.java
+++ app/src/main/java/net/marelias/notepad/web/DraggableScrollbarWebView.java
@@ -12,12 +12,6 @@ import android.webkit.WebView;
 @SuppressLint("ClickableViewAccessibility")
 public class DraggableScrollbarWebView extends WebView {
 
-    private boolean _isFastScrolling = false;
-    private boolean _fastScrollEnabled = true;
-    private boolean _ltr = true;
-    private int _thumbHeight;
-    private int _grabWidth;
-
     public DraggableScrollbarWebView(Context context) {
         super(context);
     }
@@ -28,61 +22,6 @@ public class DraggableScrollbarWebView extends WebView {
 
     public DraggableScrollbarWebView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if (!_fastScrollEnabled) {
-            return super.onInterceptTouchEvent(ev);
-        }
-        if (_isFastScrolling) {
-            return true;
-        }
-        if (ev.getActionMasked() == MotionEvent.ACTION_DOWN &&
-                ((_ltr && getWidth() - _grabWidth < ev.getX())
-                        || (!_ltr && _grabWidth > ev.getX()))) {
-            computeThumbHeight();
-            awakenScrollBars();
-            float scrollbarStartPos = (float) computeVerticalScrollOffset() / computeVerticalScrollRange() * (getHeight());
-            if (Math.abs(ev.getY() - scrollbarStartPos) < _thumbHeight) {
-                _isFastScrolling = true;
-            }
-            return true;
-        }
-        return super.onInterceptTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        int action = ev.getActionMasked();
-        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
-            _isFastScrolling = false;
-        }
-        if (_isFastScrolling && action == MotionEvent.ACTION_MOVE) {
-            scrollTo(0, (int) (((ev.getY() - _thumbHeight / 2) / (getHeight() - _thumbHeight)) * (computeVerticalScrollRange() - computeVerticalScrollExtent())));
-            return true;
-        }
-        return super.onTouchEvent(ev);
-    }
-
-    // Approximate height of thumb
-    private void computeThumbHeight() {
-        int height = (int) ((float) computeVerticalScrollExtent() * getHeight() / computeVerticalScrollRange());
-        int minHeight = getHeight() / 8;
-        if (height < minHeight) {
-            height = minHeight;
-        }
-        _thumbHeight = height;
-    }
-
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            _ltr = getLayoutDirection() == View.LAYOUT_DIRECTION_LTR;
-        }
-        final DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
-        _grabWidth = (int) (1.5 * (float) getVerticalScrollbarWidth() * displayMetrics.density);
     }
 
 }
